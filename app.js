f4bfcc2fdd5ea7491bec928a01b4c6effcc1e7d7
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-11-02 08:56:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const isLocalhost = location.hostname === "localhost" || location.hostname === "127.0.0.1";
 const serverUrl = "API_URL_HERE";
 const endpoint = isLocalhost ? "http://localhost:3000" : serverUrl;
+let selectedPostId;
 
 // === READ (GET) === //
 // get all posts
@@ -27,10 +28,33 @@ function appendPosts(postList) {
                 <img src="${post.image}">
                 <h2>${post.title}</h2>
                 <p>${post.body}</p>
+                <div class="btns">
+                    <button class="btn-update-user" onclick="getPost(${post.id})">Update</button>
+                    <button class="btn-delete-user" data-id="${post.id}">Delete</button>
+                </div>
             </article>
         `;
     }
     document.querySelector("#posts-grid").insertAdjacentHTML("afterbegin", html);
+}
+
+function getPost(id) {
+    fetch(`${endpoint}/posts/${id}`)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            displayPost(data);
+        });
+}
+
+function displayPost(post) {
+    console.log(post);
+    const form = document.querySelector("#form-update");
+    form.title.value = post.title;
+    form.body.value = post.body;
+    form.url.value = post.image;
+    form.scrollIntoView({ behavior: "smooth" });
 }
 
 // === INITIALIZE APP === //
